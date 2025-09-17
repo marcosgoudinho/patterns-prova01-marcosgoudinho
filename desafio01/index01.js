d7737beb39393153d00679326142d68c8edf9ba4
Marcos Vinicius Goudinho Da Silva <marcos.261619@alunosatc.edu.br>
2025-09-17 19:53:53 -0300

    [ADD]: Arrumando exercicio

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,23 +1,88 @@
-
-const { FabricaTemaClaro, FabricaTemaEscuro } = require('./problemAbstract.js');
-
-console.log('### EXECUTANDO DEMONSTRAÇÃO DO ABSTRACT FACTORY ###');
-
-const fabricaClara = new FabricaTemaClaro();
-
-const botaoClaro = fabricaClara.criarBotao();
-const textoClaro = fabricaClara.criarTexto();
-
-console.log(botaoClaro.descrever());
-console.log(textoClaro.descrever());
-
-console.log('---');
-
-const fabricaEscura = new FabricaTemaEscuro();
-const botaoEscuro = fabricaEscura.criarBotao();
-const textoEscuro = fabricaEscura.criarTexto();
-
-console.log(botaoEscuro.descrever());
-console.log(textoEscuro.descrever());
-
-console.log('\n Padrão Abstract Factory implementado com sucesso!\n');
+class ComponenteUI {
+    descrever() {
+      throw new Error("Este método deve ser implementado pela subclasse.");
+    }
+  }
+  
+  class Botao extends ComponenteUI {}
+  class Texto extends ComponenteUI {}
+  
+  class BotaoClaro extends Botao {
+    descrever() {
+      return "Componente: Botão | Tema: Claro";
+    }
+  }
+  
+  class TextoClaro extends Texto {
+    descrever() {
+      return "Componente: Texto | Tema: Claro";
+    }
+  }
+  
+  class BotaoEscuro extends Botao {
+    descrever() {
+      return "Componente: Botão | Tema: Escuro";
+    }
+  }
+  
+  class TextoEscuro extends Texto {
+    descrever() {
+      return "Componente: Texto | Tema: Escuro";
+    }
+  }
+  
+  class FabricaDeUI {
+    criarBotao() {
+      throw new Error("Este método deve ser implementado pela subclasse.");
+    }
+  
+    criarTexto() {
+      throw new Error("Este método deve ser implementado pela subclasse.");
+    }
+  }
+  
+  class FabricaTemaClaro extends FabricaDeUI {
+    criarBotao() {
+      return new BotaoClaro();
+    }
+  
+    criarTexto() {
+      return new TextoClaro();
+    }
+  }
+  
+  class FabricaTemaEscuro extends FabricaDeUI {
+    criarBotao() {
+      return new BotaoEscuro();
+    }
+  
+    criarTexto() {
+      return new TextoEscuro();
+    }
+  }
+  
+  module.exports = {
+    FabricaTemaClaro,
+    FabricaTemaEscuro,
+  };
+  
+  
+  console.log("Iniciando a criação de componentes de UI...");
+  
+  console.log("\n--- Usando a Fábrica do Tema Claro ---");
+  const fabricaClara = new FabricaTemaClaro();
+  const botaoClaro = fabricaClara.criarBotao();
+  const textoClaro = fabricaClara.criarTexto();
+  
+  console.log(botaoClaro.descrever());
+  console.log(textoClaro.descrever());
+  
+  console.log("\n--- Usando a Fábrica do Tema Escuro ---");
+  const fabricaEscura = new FabricaTemaEscuro();
+  const botaoEscuro = fabricaEscura.criarBotao();
+  const textoEscuro = fabricaEscura.criarTexto();
+  
+  console.log(botaoEscuro.descrever());
+  console.log(textoEscuro.descrever());
+  
+  console.log("\nCriação de componentes finalizada.");
--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,9 +1,103 @@
-const { CarroBuilder, MotoBuilder } = require('./problemBuilder.js');
+class Veiculo {
+  constructor() {
+    this.tipo = '';
+    this.motor = '';
+    this.rodas = 0;
+    this.portas = 0;
+  }
 
-console.log('### EXECUTANDO DEMONSTRAÇÃO DO BUILDER ###');
+  descrever() {
+    console.log(`
+      --- Detalhes do Veículo ---
+      Tipo: ${this.tipo}
+      Motor: ${this.motor}
+      Rodas: ${this.rodas}
+      Portas: ${this.portas || 'N/A'}
+      ---------------------------
+    `);
+  }
+}
 
+class VeiculoBuilder {
+  definirTipo() {}
+  construirMotor() {}
+  instalarRodas() {}
+  instalarPortas() {}
+  getVeiculo() {}
+}
+
+class CarroBuilder extends VeiculoBuilder {
+  constructor() {
+    super();
+    this.veiculo = new Veiculo();
+  }
+
+  definirTipo() {
+    this.veiculo.tipo = 'Carro';
+    return this; 
+  }
+
+  construirMotor() {
+    this.veiculo.motor = '1.6 V4';
+    return this;
+  }
+
+  instalarRodas() {
+    this.veiculo.rodas = 4;
+    return this;
+  }
+
+  instalarPortas() {
+    this.veiculo.portas = 4;
+    return this;
+  }
+
+  getVeiculo() {
+    return this.veiculo;
+  }
+}
+
+class MotoBuilder extends VeiculoBuilder {
+  constructor() {
+    super();
+    this.veiculo = new Veiculo();
+  }
+
+  definirTipo() {
+    this.veiculo.tipo = 'Moto';
+    return this;
+  }
+
+  construirMotor() {
+    this.veiculo.motor = '500cc';
+    return this;
+  }
+
+  instalarRodas() {
+    this.veiculo.rodas = 2;
+    return this;
+  }
+
+  instalarPortas() {
+    return this;
+  }
+
+  getVeiculo() {
+    return this.veiculo;
+  }
+}
+
+module.exports = {
+  CarroBuilder,
+  MotoBuilder,
+};
+
+
+
+console.log("Iniciando a construção de veículos com o padrão Builder...");
+
+console.log("\n--> Construindo um Carro...");
 const carroBuilder = new CarroBuilder();
-
 const carro = carroBuilder
   .definirTipo()
   .construirMotor()
@@ -11,18 +105,17 @@ const carro = carroBuilder
   .instalarPortas()
   .getVeiculo();
 
-console.log('--> Carro construído:');
 carro.descrever();
 
-const motoBuilder = new MotoBuilder();
 
+console.log("--> Construindo uma Moto...");
+const motoBuilder = new MotoBuilder();
 const moto = motoBuilder
   .definirTipo()
   .construirMotor()
   .instalarRodas()
   .getVeiculo();
 
-console.log('--> Moto construída:');
 moto.descrever();
 
-console.log('Padrão Builder implementado com sucesso!\n');
+console.log("\nConstrução de veículos finalizada.");
